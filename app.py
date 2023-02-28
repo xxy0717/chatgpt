6a2f97d72d036b1e2bd82247e8f2830ac8a0c521
xxy0717 <125196979+xxy0717@users.noreply.github.com>
2023-02-28 21:16:50 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+import openai_secret_manager
+import openai
+import streamlit as st
+
+# 配置OpenAI API Key
+openai.api_key = openai_secret_manager.get_secret("openai")["api_key"]
+MODEL_ID = "davinci"
+
+# Streamlit应用程序
+st.set_page_config(page_title="Chatbot Demo")
+
+st.title("Chatbot Demo")
+
+# 定义聊天框
+st.sidebar.subheader("Chat")
+user_input = st.sidebar.text_input("You", "")
+bot_output = st.sidebar.empty()
+
+# 与OpenAI模型进行交互，获取回复
+if st.sidebar.button("Send"):
+    # 调用OpenAI模型生成回复
+    response = openai.Completion.create(
+        engine=MODEL_ID,
+        prompt=user_input,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    # 提取回复
+    bot_response = response.choices[0].text.strip()
+    # 更新聊天框
+    bot_output.text(bot_response)
