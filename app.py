7fa708a71221f0077c8a8f3dc05ce7d460703a8a
xxy0717 <125196979+xxy0717@users.noreply.github.com>
2023-03-02 10:51:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
-import streamlit as st
 import openai
+import streamlit as st
+
+# 设置OpenAI API密钥
 openai.api_key = "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"
 
-# 定义输入框和提交按钮
+# 定义应用程序标题和文本输入框
+st.title("OpenAI文本生成应用程序")
 input_text = st.text_area("请输入文本", "")
-submit_button = st.button("提交")
 
 # 处理提交按钮点击事件
-if submit_button:
+if st.button("提交"):
     # 调用OpenAI API
+    prompt = input_text.strip()
     response = openai.Completion.create(
         engine="davinci",
-        prompt=input_text,
+        prompt=prompt,
         max_tokens=60
     )
-    # 显示结果
+    # 显示生成的文本
     st.write("生成的文本：")
-    st.write(response.choices[0].text)
+    st.write(response.choices[0].text.strip())
