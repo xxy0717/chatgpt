7c07c851081696decf234ccca93ac6affd944a8e
xxy0717 <125196979+xxy0717@users.noreply.github.com>
2023-03-02 11:23:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,30 @@
-import openai
 import streamlit as st
+import openai
+import os
 
 # 设置OpenAI API密钥
-openai.api_key = "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"
+if "openai" not in st.secrets:
+    st.error("请在Streamlit Secrets中设置OpenAI API密钥")
+else:
+    openai.api_key = st.secrets["openai"]
 
-# 定义主函数
-def main():
-    # 设置应用程序标题
-    st.title("Chat with GPT-3")
+# 输入框
+text = st.text_input("请输入文本：")
 
-    # 显示输入框，以获取用户输入
-    prompt = st.text_input("You: ", "")
+# 提交按钮
+if st.button("提交"):
+    try:
+        # 调用OpenAI API生成文本
+        response = openai.Completion.create(
+            engine="davinci",
+            prompt=text,
+            max_tokens=50,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
 
-    # 当用户点击“Send”按钮时，调用OpenAI API，并显示响应
-    if st.button("Send"):
-        try:
-            response = openai.Completion.create(
-                engine="davinci",
-                prompt=prompt,
-                max_tokens=60
-            )
-            message = response.choices[0].text.strip()
-            st.text_area("GPT-3:", message)
-        except Exception as e:
-            st.error(e)
-
-# 调用主函数
-if __name__ == "__main__":
-    main()
+        # 输出结果
+        st.write(response.choices[0].text)
+    except openai.Error as e:
+        st.error("OpenAI API调用错误：" + str(e))
