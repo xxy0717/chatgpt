c015f0114840222488c00fcb9f074d28c11f65c0
xxy0717 <125196979+xxy0717@users.noreply.github.com>
2023-03-02 11:07:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import openai
 import streamlit as st
 
 # 设置OpenAI API密钥
-openai.api_key = "zl4PpIm9MrCMdB1NN5GlT3BlbkFJCQDtIZzFhjVqfIoUJiye"
+openai.api_key = "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"
 
 # 定义主函数
 def main():
