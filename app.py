9019dbf67d0f2461a66fb28f5e505591a13fa482
xxy0717 <125196979+xxy0717@users.noreply.github.com>
2023-03-02 12:07:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -33,6 +33,6 @@ if st.button("提交"):
         st.write(response.choices[0].text)
     except openai.Error as e:
 #        st.error("OpenAI API调用错误：" + str(e))
-    print("OpenAI API调用错误：", e)
+        print("OpenAI API调用错误：", e)
 
         
